8267ade57d1817f3c651482cb628c423481baa66
Rakesh <rockersinfo@gmail.com>
2024-04-21 10:06:57 +0530

    update

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     author_email="rockersinfo@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.7",
+    python_requires=">=3.12",
     install_requires=LIST_OF_REQUIREMENTS
 )
